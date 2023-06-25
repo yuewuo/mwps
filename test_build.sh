c798b12929f3931776e5457208127e768e9e7c26
Yue Wu <yue.wu@yale.edu>
2023-06-25 14:22:47 -0700

    add test build script

---
+++ test_build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ex
+
+cargo clean
+cargo clippy -- -Dwarnings  # A collection of lints to catch common mistakes and improve your Rust code.
+
+# check this first because it's easy to have errors
+cargo test --no-run --features u32_index
+cargo test --no-run --features u32_index --release
+
+cargo test --no-run
+cargo test --no-run --release
+cargo test --no-run --features r64_weight
+cargo test --no-run --features r64_weight --release
+# cargo test --no-run --features python_binding
+# cargo test --no-run --features python_binding --release
