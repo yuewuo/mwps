2466d98ca0ac3940d8eed24fd4251addbcf4f1d6
Yue Wu <yue.wu@yale.edu>
2024-11-04 14:10:53 -0600

    add pyproject-rational for rational build

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate clap;
 
 use crate::clap::Parser;
-use mwpf::cli::*;
+use crate::cli::*;
 
 pub fn main() {
     #[cfg(all(feature = "slp", feature = "incr_lp"))]
---
+++ src/python/mwpf_rational/__init__.py
@@ -0,0 +1,5 @@
+from .mwpf_rational import *
+
+__doc__ = mwpf_rational.__doc__
+if hasattr(mwpf_rational, "__all__"):
+    __all__ = mwpf_rational.__all__
